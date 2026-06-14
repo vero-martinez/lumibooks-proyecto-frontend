d661d4cbc6091ded21e1c0f8d1ec866019b93931
vero-martinez <veris.14@hotmail.com>
2026-06-14 01:04:12 -0500

    style: configurar fuente global geist sans en el body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className={`${geistSans.className} min-h-full flex flex-col`}>
         <Providers>{children}</Providers>
       </body>
     </html>
