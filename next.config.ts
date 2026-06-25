534ad749abf4204cb659ebbc3b6f286d3deee099
vero-martinez <veris.14@hotmail.com>
2026-06-25 00:33:03 -0500

    feat: habilitar carga de imágenes remotas desde Cloudinary en Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+// Configuración global de Next.js
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Permite que el componente Next/Image cargue imágenes
+   * alojadas en Claudinary de forma segura y optimizada
+   */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
